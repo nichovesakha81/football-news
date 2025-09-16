15f6029fefb3c30736640c3e75324a8d5165520c
nichovesakha81 <studynicho@gmail.com>
2025-09-16 23:40:58 +0700

    Trigger rebuild after updating env.prod. with new credential

--- main/models.py
+++ main/models.py
@@ -11,6 +11,7 @@ class News(models.Model):
         ('analysis', 'Analysis'),
     ]
     
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     content = models.TextField()
