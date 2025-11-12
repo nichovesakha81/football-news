66f0498e2257044e24d0f6735619213e7d205aad
nichovesakha81 <studynicho@gmail.com>
2025-11-12 19:27:23 +0700

    tutorial 8 selesai

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from main.views import (
     show_main, create_news, show_news, show_xml,
     show_json, show_xml_by_id, show_json_by_id, 
     register, login_user, logout_user,
-    edit_news, delete_news, add_news_entry_ajax
+    edit_news, delete_news, add_news_entry_ajax,
+    proxy_image, create_news_flutter,
 )
 
 app_name = 'main'
@@ -26,4 +27,6 @@ urlpatterns = [
     path('news/<uuid:id>/delete', delete_news, name='delete_news'),
     
     path('create-news-ajax', add_news_entry_ajax, name='add_news_entry_ajax'),
+    path('proxy-image/', proxy_image, name='proxy_image'),
+    path('create-flutter/', create_news_flutter, name='create_news_flutter'),
 ]
